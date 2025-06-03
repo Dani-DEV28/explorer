3a1b4906b26a6ed49fee9e411846eab6895c10ed
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-06-02 19:16:46 -0700

    modified salemander

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -32,6 +32,68 @@ public class ExplorerSearch {
         // Implement your method here!
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
-        return -1;
+        boolean[][] visited = new boolean[island.length][island[0].length];
+        int[] start = islanderLocation(island);
+
+        return reachableArea(island, visited, start);
+    }
+
+    public static int reachableArea(int[][] island, boolean[][] visited, int[] current) {
+        int curR = current[0];
+        int curC = current[1];
+
+        if (curR < 0 || curR >= island.length || curC < 0 || curC >= island[0].length) return 0;
+        if (visited[curR][curC]) return 0;
+        if (island[curR][curC] == 2 || island[curR][curC] == 3) return 0;
+
+        visited[curR][curC] = true;
+
+        int space = 1;
+
+        List<int[]> neighbors = possibleMoves(island, current);
+
+        for (int[] neighbor : neighbors) {
+            space += reachableArea(island, visited, neighbor);
+        }
+
+        return space;
+    }
+
+    public static List<int[]> possibleMoves(int[][] enclosure, int[] currentLocation) {
+        List<int[]> moves = new ArrayList<>();
+
+        int curR = currentLocation[0];
+        int curC = currentLocation[1];
+
+        int[][] directions = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
+
+        for (int[] d : directions) {
+            int newR = curR + d[0];
+            int newC = curC + d[1];
+            if (newR >= 0 && newR < enclosure.length && newC >= 0 && newC < enclosure[0].length) {
+                if (enclosure[newR][newC] != 2 && enclosure[newR][newC] != 3) {
+                    moves.add(new int[]{newR, newC});
+                }
+            }
+        }
+
+        return moves;
+    }
+
+    public static int[] islanderLocation(int[][] island) {
+        for (int i = 0; i < island.length; i++) {
+            for (int j = 0; j < island[i].length; j++) {
+                if (island[i][j] == 0) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("No explorer starting point (value 0) found");
     }
 }
